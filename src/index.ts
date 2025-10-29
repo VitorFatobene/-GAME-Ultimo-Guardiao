84f3642a76a1853b8460b2949be0fa6cbbdf0d29
Vitor Fatobene <vitorfatobene@gmail.com>
2025-10-29 20:12:48 -0300

    verificação de classe selecionada implementada

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import { Mago } from "./classes/entidades/Mago";
 import { Necromante } from "./classes/entidades/Necromante";
 
 const imagens = document.querySelectorAll<HTMLImageElement>(".imagens")
-
+const botao = document.getElementById("escolher") as HTMLButtonElement
 const guerreiro = new Guerreiro("Vitor", 40, 2, 100, 50, "Espada do Deus do Trovão", 8);
 const mago = new Mago("maguinho", 30, 20, 65, 40,"Cajado Lendário do dragão", 7);
 const necromante = new Necromante("Necromante", 25, 25, 50, 35,"Varinha Do Rei Antigo", 9);
+let classeSelecionada: boolean = false;
+sessionStorage.setItem("nome", "Vitor")
 console.log(guerreiro)
 console.log(mago)
 console.log(necromante)
@@ -17,6 +19,7 @@ imagens.forEach(function (img){
     img.addEventListener("click", function(){
         removerClicada()
         img.classList.add("selecionada")
+        classeSelecionada = true;
     })
 })
 
@@ -25,5 +28,13 @@ function removerClicada() {
         i.classList.remove("selecionada");
     });
 }
+
+botao.addEventListener("click", function(){
+    if(classeSelecionada == false){
+        alert("Por favor, selecione uma classe")
+    }else{
+        window.location.href = "/html/combate.html"
+    }
+})
 
 
